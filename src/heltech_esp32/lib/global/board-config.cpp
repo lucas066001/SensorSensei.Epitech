b02540f5368acca65758c8b74f3c1cfcaade0426
lucas066001 <chapuis863@gmail.com>
2025-05-23 07:41:41 +0200

    feat(heltec): Adding global configuration file

---
+++ src/heltech_esp32/lib/global/board-config.cpp
@@ -0,0 +1,7 @@
+#include <board-config.h>
+
+namespace SensorSensei::Constants::BoardConfig
+{
+    double SleepTime = 10 * 1000000ULL; // 10 seconds in microseconds
+    // double SleepTime = 5 * 60 * 1000000ULL; // 5 minutes in microseconds
+}
---
+++ src/heltech_esp32/lib/global/board-config.h
@@ -0,0 +1,11 @@
+#ifndef BOARD_CONFIG_CONSTANTS_H
+#define BOARD_CONFIG_CONSTANTS_H
+
+#pragma once
+
+namespace SensorSensei::Constants::BoardConfig
+{
+    extern double SleepTime; // Number of retry before passing task
+}
+
+#endif
