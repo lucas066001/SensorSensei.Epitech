bae9d54916d3c838da67a1c29aa0980806bdd086
lucas066001 <chapuis863@gmail.com>
2025-06-06 15:25:37 +0200

    feat(gateway): Implementing overall board behavior above all previous services

--- src/lilygo_gateway/src/main.cpp
+++ src/lilygo_gateway/src/main.cpp
@@ -1,12 +1,69 @@
-#include "services/gps_service/gps_service.h"
-#include "services/lora_service/lora_service.h"
+#include <gps/gps.h>
+#include <wifi/wifi.h>
+#include <lora/lora-receiver.h>
+#include <utils/parser/sensor-parser.h>
+#include <external/sensor-community/sensor-community.h>
 
-void setup(){
-  // setup_lora();
-  setup_gps();
+using namespace SensorSensei::Services;
+
+GpsService gpsService;
+WifiService wifiService("YOUR_WIFI_SSID", "YOUR_WIFI_PASSWORD");
+LoraReceiverService loraReceiverService;
+External::SensorCommunityService sensorCommunityService(&wifiService);
+Utils::SensorParserService sensorParserService;
+
+void setup()
+{
+	Serial.begin(115200);
+	delay(1000);
+	wifiService.connect();
+	gpsService.setup_gps();
+	loraReceiverService.setup_lora();
 }
 
-void loop(){
-  // loop_lora();
-  loop_gps();
+void loop()
+{
+	String receivedPacket = loraReceiverService.handle();
+	if (!receivedPacket.isEmpty())
+	{
+		Serial.println("Received Packet: " + receivedPacket);
+
+		// Basically useless as sensorCommunity doesn't allow GPS data update through API
+		gpsService.update_spatio_data();
+		std::tuple<float, float, float> sensorDatas = sensorParserService.extractSensorData(receivedPacket);
+
+		float meanSoundLevel = std::get<0>(sensorDatas);
+		float meanDustDensity25 = std::get<1>(sensorDatas);
+		float meanDustDensity10 = std::get<2>(sensorDatas);
+
+		Serial.printf("Parsed meanSoundLevel: %d\n", meanSoundLevel);
+		Serial.printf("Parsed meanDustDensity25: %d\n", meanDustDensity25);
+		Serial.printf("Parsed meanDustDensity10: %d\n", meanDustDensity10);
+
+		if (meanSoundLevel >= 0.0f)
+		{
+			sensorCommunityService.sendSoundData(meanSoundLevel);
+		}
+		else
+		{
+			// Low sound level so we round to 0.0f
+			sensorCommunityService.sendSoundData(0.0f);
+			Serial.println("Extremly low sound data received. sending 0.0f");
+		}
+
+		if (meanDustDensity10 >= 0.0f && meanDustDensity25 >= 0.0f)
+		{
+			sensorCommunityService.sendDustData(meanDustDensity25, meanDustDensity10);
+		}
+		else
+		{
+			Serial.println("Invalid dust data received. Ignoring");
+		}
+	}
+	else
+	{
+		// Serial.println("No packet received");
+	}
+
+	// delay(1000);
 }
